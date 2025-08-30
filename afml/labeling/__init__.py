c9316f7f7b52a1c334927912a5e11279bb47821b
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2025-08-30 04:17:39 +0300

    Renamed trend meta-labeling function to `get_trend_scanning_meta_labels`

--- afml/labeling/__init__.py
+++ afml/labeling/__init__.py
@@ -1,6 +1,6 @@
 from .fixed_time_horizon import fixed_time_horizon
 from .performance_metrics import calculate_label_metrics
-from .trend_scanning import get_bins_from_trend, trend_meta_labels
+from .trend_scanning import get_bins_from_trend, get_trend_scanning_meta_labels
 from .triple_barrier import (
     add_vertical_barrier,
     drop_labels,
