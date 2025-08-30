0c835b2a6953054ac29bb53599832f090aa167a8
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2025-08-30 15:38:26 +0300

    Removed get_data.py

--- afml/util/__init__.py
+++ afml/util/__init__.py
@@ -14,13 +14,6 @@ from .constants import (
     PERCENTILES,
     UTC,
 )
-from .get_data import (
-    get_ticks,
-    load_tick_data,
-    login_mt5,
-    save_data_to_parquet,
-    verify_or_create_account_info,
-)
 from .misc import (
     DataFrameFormatter,
     _count_max_decimals_numba,
