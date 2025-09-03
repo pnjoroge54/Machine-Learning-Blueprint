b73bf222f0c4249324b2577d8d194ac3aed00c05
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2025-09-03 20:18:20 +0300

    Update optimized_concurrent.py
    Made verbose default=False

--- afml/sampling/optimized_concurrent.py
+++ afml/sampling/optimized_concurrent.py
@@ -292,7 +292,7 @@ def get_av_uniqueness_from_triple_barrier_optimized(
     triple_barrier_events,
     close_series,
     num_conc_events=None,
-    verbose=True,
+    verbose=False,
 ):
     """
     Optimized orchestrator for deriving average sample uniqueness from triple barrier events.
@@ -321,7 +321,7 @@ def get_av_uniqueness_from_triple_barrier_optimized(
         Close prices
     num_conc_events : pd.Series, optional
         Precomputed concurrent events count. If None, will be computed.
-    verbose : bool, default=True
+    verbose : bool, default=False
         Report progress on parallel jobs
 
     Returns:
