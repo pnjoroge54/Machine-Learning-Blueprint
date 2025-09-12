b5a5b4e3bd8550dc6983547290e7bfa749e69e52
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2025-09-12 06:41:14 +0300

    Added modules

--- afml/backtest_statistics/__init__.py
+++ afml/backtest_statistics/__init__.py
@@ -2,10 +2,13 @@
 Implements general backtest statistics
 """
 
+from .backtester import backtest_model, backtest_with_labeling
 from .backtests import CampbellBacktesting
 from .log_analyser import load_model_logs
 from .performance_analysis import (
     calculate_performance_metrics,
+    get_annualization_factors,
+    get_trades,
     run_meta_labeling_analysis,
 )
 from .primary_model_experiments import (
@@ -18,8 +21,12 @@ from .primary_model_experiments import (
     train_my_model,
 )
 from .reporting import (
+    compare_pr_curves,
     compare_roc_curves,
+    compare_roc_pr_curves,
     create_classification_report_image,
+    meta_labelling_classification_report_images,
+    meta_labelling_classification_report_tables,
     meta_labelling_reports,
 )
 from .research_framework import ExperimentHook, ExperimentRunner, ResearchExperiment
