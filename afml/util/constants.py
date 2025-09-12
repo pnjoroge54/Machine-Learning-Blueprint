160b4dace1db3856c2557a78c7d44f096029d11a
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2025-09-12 06:29:26 +0300

    Converted tuples to lists

--- afml/util/constants.py
+++ afml/util/constants.py
@@ -5,10 +5,10 @@ from pytz import timezone
 
 DATA_PATH = Path.home() / "tick_data_parquet"
 CLEAN_DATA_PATH = Path.home() / "tick_data_clean_parquet"
-OHLCV = ("open", "high", "low", "close", "volume")
-DATE_COMPONENTS = ("year", "month", "day", "hour", "minute", "second", "microsecond")
+OHLCV = ["open", "high", "low", "close", "volume"]
+DATE_COMPONENTS = ["year", "month", "day", "hour", "minute", "second", "microsecond"]
 UTC = timezone("UTC")
-PERCENTILES = (0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99)
+PERCENTILES = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99]
 NUM_THREADS = cpu_count() - 1
 GREEKS = ["β", "γ", "ρ", "φ", "χ", "δ", "α", "σ", "λ", "μ", "τ", "θ", "ε", "ψ"]
 
