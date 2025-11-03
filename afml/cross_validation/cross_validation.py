487aefdcc4091748b3de795058002b704aebe226
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2025-11-04 00:40:43 +0300

    Updates

--- afml/cross_validation/cross_validation.py
+++ afml/cross_validation/cross_validation.py
@@ -249,10 +249,9 @@ def ml_cross_val_score(
     if seq_bootstrap:
         t1 = classifier.samples_info_sets.copy()
         common_idx = t1.index.intersection(y.index)
-        iloc = t1.index.searchsorted(common_idx[[0, -1]])
         X, y, t1 = X.loc[common_idx], y.loc[common_idx], t1.loc[common_idx]
-        sample_weight_train = sample_weight_train[iloc[0] : iloc[1] + 1]
-        sample_weight_score = sample_weight_score[iloc[0] : iloc[1] + 1]
+        sample_weight_train = pd.Series(sample_weight_train).loc[common_idx]
+        sample_weight_score = pd.Series(sample_weight_score).loc[common_idx]
         if t1.empty:
             raise KeyError(f"samples_info_sets not aligned with data")
         classifier.set_params(oob_score=False)
@@ -374,10 +373,9 @@ def analyze_cross_val_scores(
     if seq_bootstrap:
         t1 = classifier.samples_info_sets.copy()
         common_idx = t1.index.intersection(y.index)
-        iloc = t1.index.searchsorted(common_idx[[0, -1]])
         X, y, t1 = X.loc[common_idx], y.loc[common_idx], t1.loc[common_idx]
-        sample_weight_train = sample_weight_train[iloc[0] : iloc[1] + 1]
-        sample_weight_score = sample_weight_score[iloc[0] : iloc[1] + 1]
+        sample_weight_train = pd.Series(sample_weight_train).loc[common_idx]
+        sample_weight_score = pd.Series(sample_weight_score).loc[common_idx]
         if t1.empty:
             raise KeyError(f"samples_info_sets not aligned with data")
         classifier.set_params(oob_score=False)
