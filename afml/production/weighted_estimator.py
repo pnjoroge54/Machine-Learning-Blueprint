ed1fe5a1c858ad4185802f7728d567613660b7a3
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2026-03-20 02:25:21 +0300

    Update weighted_estimator.py

--- afml/production/weighted_estimator.py
+++ afml/production/weighted_estimator.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
+
+from ..sample_weights.optimized_attribution import get_weights_by_time_decay_optimized
 
 
 class _WeightedEstimator(BaseEstimator, ClassifierMixin):
