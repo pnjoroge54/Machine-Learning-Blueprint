b1305764854250a57f7134d6abed517f21dafc10
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2026-03-22 22:28:08 +0300

    Update model_development.py
    Fixed error with setting max_samples

--- afml/production/model_development.py
+++ afml/production/model_development.py
@@ -889,7 +889,8 @@ class ModelDevelopmentPipeline:
             # max_samples is a bootstrap parameter specific to RandomForestClassifier.
             # DecisionTreeClassifier has no bootstrap step and no max_samples parameter.
             av_uniqueness = self.events['tW'].mean().round(2)
-            pipe = set_pipeline_params(pipe, max_samples=av_uniqueness)
+            if pipe.steps[-1][-1].bootstrap:
+                pipe = set_pipeline_params(pipe, max_samples=av_uniqueness)
             if self.model_params.get("use_optuna", False) or self.model_params.get("rnd_search_iter", 0) > 0:
                 self.model_params["param_grid"]["max_samples"] = uniform(av_uniqueness, 1 - av_uniqueness)
 
