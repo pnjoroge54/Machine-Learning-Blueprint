fa63b33dad999ff3d931ebc95b8bab9ba26e747d
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2026-03-30 22:44:48 +0300

    Update __init__.py

--- afml/strategies/__init__.py
+++ afml/strategies/__init__.py
@@ -3,13 +3,6 @@ from .bollinger_features import (
     plot_bbands,
     plot_bbands_dual_bbp_bw,
 )
-from .genetic_optimizer import (
-    ParetoOptimizer,
-    SingleObjectiveOptimizer,
-    TripleBarrierEvaluator,
-    get_optimal_triple_barrier_labels,
-    select_knee_point,
-)
 from .ma_crossover_feature_engine import ForexFeatureEngine
 from .ma_whipsaw_ratio import (
     calculate_enhanced_whipsaw_metrics,
