3e5c8b2efba1c35ce4df713080e3f513aa2e33ad
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2026-04-10 17:55:40 +0300

    Update __init__.py

--- afml/labeling/__init__.py
+++ afml/labeling/__init__.py
@@ -1,11 +1,10 @@
-from .fixed_time_horizon import fixed_time_horizon
-from .tail_sets import TailSetLabels
+from .excess_over_mean import excess_over_mean
+from .excess_over_median import excess_over_median
 from .fixed_time_horizon import fixed_time_horizon
 from .matrix_flags import MatrixFlagLabels
-from .excess_over_median import excess_over_median
 from .raw_return import raw_return
 from .return_vs_benchmark import return_over_benchmark
-from .excess_over_mean import excess_over_mean
+from .tail_sets import TailSetLabels
 from .trend_scanning import plot_trend_labels, trend_scanning_labels
 from .triple_barrier import (
     add_vertical_barrier,
