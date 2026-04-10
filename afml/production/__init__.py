f4c7811ce923fba2e489e0a9e444a458266bbc72
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2026-04-10 21:41:13 +0300

    Update __init__.py

--- afml/production/__init__.py
+++ afml/production/__init__.py
@@ -5,12 +5,12 @@ from .model_export import (
     extract_onnx_metadata,
     validate_onnx_predictions,
 )
-# from .model_development import (
-#     calculate_rolling_metrics,
-#     create_feature_engineering_pipeline,
-#     generate_events_triple_barrier,
-#     load_and_prepare_training_data,
-#     ModelDevelopmentPipeline,
-# )
-# from .dual_model_development import BidAskLongShortPipeline
+from .model_development import (
+    calculate_rolling_metrics,
+    create_feature_engineering_pipeline,
+    generate_events_triple_barrier,
+    load_and_prepare_training_data,
+    ModelDevelopmentPipeline,
+)
+from .dual_model_development import BidAskLongShortPipeline
 
