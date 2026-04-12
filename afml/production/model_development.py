bdf195497f8d07f9aba28ffe64ceab37df302e25
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2026-04-12 15:28:00 +0300

    Update model_development.py
    Changed logging of sample_weights

--- afml/production/model_development.py
+++ afml/production/model_development.py
@@ -527,18 +527,13 @@ def get_optimal_sample_weight(
     cv_results = pd.DataFrame()
     scoring    = "f1" if set(y.unique()) == {0, 1} else "neg_log_loss"
     
-    pbar1 = tqdm(weights.items(), desc="Analyzing weighting schemes", total=len(weights), mininterval=0.5)
+    pbar1 = tqdm(weights.items(), desc="Analyzing weighting schemes", total=len(weights), mininterval=1.0, leave=False)
     for i, (scheme, weight) in enumerate(pbar1, 1):
         best_score, best_scheme, cv_results = best_weighting_scheme(
             clone(classifier), X, y, cv_gen, scoring, weight, weights["return"],
             scheme, best_score, best_scheme, cv_results,
         )
         pbar1.set_postfix({"scheme": scheme, "scoring": scoring, "score": f"{cv_results[scheme].mean():.4f}"})
-        if i == len(weights):
-            pbar1.set_postfix({
-                "best": best_scheme,
-                "score": f"{best_score:.4f}"
-            })
             
     best_weight  = weights[best_scheme]
     linear_search = [1, 0] if linear is None else ([1] if linear else [0])
@@ -556,18 +551,20 @@ def get_optimal_sample_weight(
             scheme = f"{best_scheme}_{'linear' if lin else 'exp'}_{decay}"
             time_decay_weights[scheme] = best_weight * decay_vec
 
-    pbar2 = tqdm(time_decay_weights.items(), desc=f"Analyzing time-decay", total=len(time_decay_weights), mininterval=0.5)
+    pbar2 = tqdm(time_decay_weights.items(), desc=f"Analyzing time-decay", total=len(time_decay_weights), mininterval=1.0)
     for i, (scheme, weight) in enumerate(pbar2, 1):
         best_score, best_scheme, cv_results = best_weighting_scheme(
             clone(classifier), X, y, cv_gen, scoring, weight, weights["return"],
             scheme, best_score, best_scheme, cv_results,
         )
         pbar2.set_postfix({"scheme": scheme, "scoring": scoring, "score": f"{cv_results[scheme].mean():.4f}"})
-        if i == len(time_decay_weights):
-            pbar2.set_postfix({
-                "best": best_scheme,
-                "score": f"{best_score:.4f}"
-            })
+
+    logger.info(f'{pformat(
+        {"best": best_scheme,
+         "scoring": scoring,
+         "score": float(best_score.round(4))
+        })'
+    )
         
     weights.update(time_decay_weights)
     cv_results_dict = {
